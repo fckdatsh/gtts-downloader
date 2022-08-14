24e186a6b0d303009e9b71c6fe48bf1c9563027d
Rob <robertdacorte@gmail.com>
2022-08-14 13:40:25 +0200

    fix url condition

--- gtts-downloader.js
+++ gtts-downloader.js
@@ -5,7 +5,7 @@ chrome.devtools.network.onRequestFinished.addListener(request => {
     errorDiv.style.display = 'none';
     if (!!body) {
       if (request.request && request.request.url) {
-        if (request.request.url.includes('https://texttospeech.googleapis.com')) {
+        if (request.request.url.includes('texttospeech.googleapis.com')) {
           document.querySelector('a#download').href = "data:audio/wav; base64, " + JSON.parse(body).audioContent;
           document.querySelector('a#download').style.display = 'block';
           document.querySelector('#imageDance').style.display = 'block';
