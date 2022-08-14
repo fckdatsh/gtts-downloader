9e083984d2a73bc07809e510c0024bc299e36f1f
Rob <robertdacorte@gmail.com>
2022-08-14 13:53:26 +0200

    small refactor

--- gtts-downloader.js
+++ gtts-downloader.js
@@ -3,19 +3,17 @@ chrome.devtools.network.onRequestFinished.addListener(request => {
   errorDiv.style.display = 'none';
   request.getContent((body) => {
     errorDiv.style.display = 'none';
-    if (!!body) {
-      if (request.request && request.request.url) {
-        if (request.request.url.includes('texttospeech.googleapis.com')) {
-          document.querySelector('a#download').href = "data:audio/wav; base64, " + JSON.parse(body).audioContent;
-          document.querySelector('a#download').style.display = 'block';
-          document.querySelector('#imageDance').style.display = 'block';
-          chrome.runtime.sendMessage({
-            response: JSON.parse(body)
-          });
-        }
+    if (request.request && request.request.url && request.request.url.includes('texttospeech.googleapis.com')) {
+      if (!!body) {
+        document.querySelector('a#download').href = "data:audio/wav; base64, " + JSON.parse(body).audioContent;
+        document.querySelector('a#download').style.display = 'block';
+        document.querySelector('#imageDance').style.display = 'block';
+        chrome.runtime.sendMessage({
+          response: JSON.parse(body)
+        });
+      } else {
+        errorDiv.style.display = 'block';
       }
-    } else {
-      errorDiv.style.display = 'block';
     }
   });
 });
