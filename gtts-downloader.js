0a29828d53157d63308ecc6d08d1bfbf7c0e261c
Rob <robertdacorte@gmail.com>
2022-08-14 13:21:54 +0200

    added validation for response higher than 10mb

--- gtts-downloader.js
+++ gtts-downloader.js
@@ -1,14 +1,21 @@
 chrome.devtools.network.onRequestFinished.addListener(request => {
+  const errorDiv = document.querySelector('div#error');
+  errorDiv.style.display = 'none';
   request.getContent((body) => {
-    if (request.request && request.request.url) {
-      if (request.request.url.includes('https://texttospeech.googleapis.com')) {
-		document.querySelector('a#download').href = "data:audio/wav; base64, " + JSON.parse(body).audioContent;
-		document.querySelector('a#download').style.display = 'block';
-		document.querySelector('#imageDance').style.display = 'block';
-        chrome.runtime.sendMessage({
+    errorDiv.style.display = 'none';
+    if (!!body) {
+      if (request.request && request.request.url) {
+        if (request.request.url.includes('https://texttospeech.googleapis.com')) {
+          document.querySelector('a#download').href = "data:audio/wav; base64, " + JSON.parse(body).audioContent;
+          document.querySelector('a#download').style.display = 'block';
+          document.querySelector('#imageDance').style.display = 'block';
+          chrome.runtime.sendMessage({
             response: JSON.parse(body)
-        });
+          });
+        }
       }
+    } else {
+      errorDiv.style.display = 'block';
     }
   });
 });
